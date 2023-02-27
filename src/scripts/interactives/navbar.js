80e6c34571999d851e6749803b6266fc5a920e0b
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2023-02-27 23:51:33 +0700

    fix(nav): add button close drawer nav mobile

--- src/scripts/interactives/navbar.js
+++ src/scripts/interactives/navbar.js
@@ -5,11 +5,13 @@ const drawer = document.querySelector(".mobile-nav");
 button.addEventListener("click", (e) => {
   e.preventDefault();
   drawer.classList.toggle("open");
-  e.stopPropagation();
-});
 
-body.addEventListener("click", (e) => {
-  e.preventDefault();
-  drawer.classList.remove("open");
+  const isOpen = drawer.classList.contains("open");
+  if (isOpen) {
+    button.innerHTML = "X";
+  } else {
+    button.innerHTML = "☰";
+  }
+
   e.stopPropagation();
 });
